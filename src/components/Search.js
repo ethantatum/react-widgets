aff1f448a98dac95fbb43d6471769e834bac5fec
Ethan Tatum <ethantatum81@gmail.com>
2020-09-02 10:11:02 -0700

    added comments around useEffect

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,6 +2,16 @@ import React, { useState, useEffect } from "react";
 
 const Search = () => {
   const [textInput, setTextInput] = useState("");
+
+  // First argument of useEffect is always a function
+  // Second arugment of useEffect is empty array, array with values, or nothing
+  // [] means run at initial render,
+  // nothing means run at initial render and after every rerender
+  // [data] means run at initial render, and after every rerender IF data has changed since last render
+  useEffect(() => {
+    console.log("useEffect fired");
+  }, []);
+
   return (
     <div className="ui container">
       <div className="ui form">
