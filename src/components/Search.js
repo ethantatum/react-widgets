9237645dc1b627b7a2770c08ce4107dac42111e5
Ethan Tatum <ethantatum81@gmail.com>
2020-09-02 22:30:14 -0700

    finalized useEffect on Search component

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,16 +1,35 @@
 import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const Search = () => {
   const [textInput, setTextInput] = useState("");
+  const [results, setResults] = useState([]);
 
-  // First argument of useEffect is always a function
+  // First argument of useEffect is always a function (but can't use 'async/await')
   // Second arugment of useEffect is empty array, array with values, or nothing
   // [] means run at initial render,
   // nothing means run at initial render and after every rerender
   // [data] means run at initial render, and after every rerender IF data has changed since last render
+
   useEffect(() => {
-    console.log("useEffect fired");
-  }, []);
+    const search = async () => {
+      const { data } = await axios.get("https://en.wikipedia.org/w/api.php", {
+        params: {
+          action: "query",
+          list: "search",
+          origin: "*",
+          format: "json",
+          srsearch: textInput,
+        },
+      });
+
+      setResults(data.query.search);
+    };
+    if (textInput) {
+      search();
+    }
+  }, [textInput]);
+
 
   return (
     <div className="ui container">
