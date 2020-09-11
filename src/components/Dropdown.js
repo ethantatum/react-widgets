5a1370c9e1740e91412bec1809dc8d4872d457f8
Ethan Tatum <ethantatum81@gmail.com>
2020-09-11 13:05:16 -0700

    finished dropdown

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,7 +1,27 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 
 const Dropdown = ({ options, selected, onSelectedChange }) => {
   const [open, setOpen] = useState(false);
+  const ref = useRef();
+
+  // Adding DOM event listener to capture whenever a user clicks anywhere on the page
+  // // Keep in mind that addEventListener always gets invoked prior to React events
+  useEffect(() => {
+    const onBodyClick = (event) => {
+      if (ref.current.contains(event.target)) {
+        return;
+      }
+      setOpen(false);
+    };
+
+    document.body.addEventListener("click", onBodyClick);
+
+    // Add cleanup function to avoid ref error if Dropdown isn't showing
+    return () => {
+      document.body.removeEventListener("click", onBodyClick);
+    };
+  }, []);
+
   const renderedOptions = options.map((option) => {
     if (option.value === selected.value) {
       return null;
@@ -18,7 +38,7 @@ const Dropdown = ({ options, selected, onSelectedChange }) => {
   });
 
   return (
-    <div className="ui form">
+    <div ref={ref} className="ui form">
       <div className="field">
         <label className="label">Select a Color</label>
         <div
