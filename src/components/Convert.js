43db02569a5a28fc27420ca7786995e4b5d2c0d4
Ethan Tatum <ethantatum81@gmail.com>
2020-09-14 11:49:28 -0700

    added useEffect debouncing to Convert

--- src/components/Convert.js
+++ src/components/Convert.js
@@ -1,10 +1,45 @@
 import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const Convert = ({ language, text }) => {
+  const [translated, setTranslated] = useState("");
+  const [debouncedText, setDebouncedText] = useState(text);
+
   useEffect(() => {
-      console.log("New language/text");
-  }, [language, text]);
-  return <div />;
+    const timerId = setTimeout(() => {
+      setDebouncedText(text);
+    }, 500);
+
+    return () => {
+      clearTimeout(timerId);
+    };
+  }, [text]);
+
+  useEffect(() => {
+    const doTranslation = async () => {
+      const { data } = await axios.post(
+        "https://translation.googleapis.com/language/translate/v2",
+        // Second object is for sending information from the req.body, but this API wants them in third object (params)
+        {},
+        {
+          params: {
+            q: debouncedText,
+            target: language.value,
+            key: "AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM",
+          },
+        }
+      );
+
+      setTranslated(data.data.translations[0].translatedText);
+    };
+
+    doTranslation();
+  }, [language, debouncedText]);
+  return (
+    <div>
+      <h1 className="ui header">{translated}</h1>
+    </div>
+  );
 };
 
 export default Convert;
